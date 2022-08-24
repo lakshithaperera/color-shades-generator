88ea143100c897e9907ac3023bfc05f4b86b6845
ITT1819_063 LakshithaPerera <pasindulakshitha33@gmail.com>
2022-08-24 19:53:57 +0530

    Updates

---
+++ precache-manifest.567c6b323aa9f0a9591daa1c128a91b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13346debbcc137cc4836986f5e4964a",
+    "url": "/color-shades-generator/index.html"
+  },
+  {
+    "revision": "6147a79e5997f5d9d4ff",
+    "url": "/color-shades-generator/static/css/main.7211de64.chunk.css"
+  },
+  {
+    "revision": "ad4df7224736efd1468d",
+    "url": "/color-shades-generator/static/js/2.db4accaa.chunk.js"
+  },
+  {
+    "revision": "85e50a0285581219fe721d1b9966173b",
+    "url": "/color-shades-generator/static/js/2.db4accaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6147a79e5997f5d9d4ff",
+    "url": "/color-shades-generator/static/js/main.b6f4a951.chunk.js"
+  },
+  {
+    "revision": "f7461ad61a0804b9e8db",
+    "url": "/color-shades-generator/static/js/runtime-main.ed1b5624.js"
+  }
+]);
